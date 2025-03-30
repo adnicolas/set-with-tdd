66c4cf909eb40a828188aab2e0fc5dc8a0c8c964
Adrián Nicolás <a.nicolaspenela@celonis.com>
2025-03-30 14:33:11 +0200

    use stream

--- src/main/java/es/geeksusma/set/ArraySet.java
+++ src/main/java/es/geeksusma/set/ArraySet.java
@@ -1,55 +1,42 @@
 package es.geeksusma.set;
 
+import java.util.Arrays;
+
 public class ArraySet {
   private Integer[] array = new Integer[0];
 
-  public Boolean isEmpty() {
-    return size() == 0;
-  }
   public Integer size() {
     return array.length;
   }
 
+  public Boolean isEmpty() {
+    return size() == 0;
+  }
+
   public Boolean contains(Integer item) {
-    // TODO: Niveles de indentación
-    for (int i = 0; i < array.length; i++) {
-      if (array[i] == item) {
-        return true;
-      }
-    }
-    return false;
+    return Arrays.stream(array).anyMatch(elem -> elem.equals(item));
   }
 
   public void add(Integer item) {
     if (contains(item)) {
       return;
     }
-    // Nuevo array con capacidad para el nuevo elemento
     Integer[] newArray = new Integer[array.length + 1];
-    // Copio los elementos preexistentes
+
     for (int i = 0; i < array.length; i++) {
       newArray[i] = array[i];
     }
-    // Asigno el nuevo
-    newArray[array.length] = item;
 
+    newArray[array.length] = item;
     array = newArray;
   }
 
-  public  Boolean remove(Integer item) {
+  public Boolean remove(Integer item) {
     if (!contains(item)) {
       return false;
     }
-    Integer[] newArray = new Integer[array.length - 1];
-    // TODO: Niveles de indentación
-    for (int i = 0; i < array.length; i++) {
-      for (int j = 0; j < newArray.length; j++) {
-        if (array[i] != item) {
-          newArray[j] = array[i];
-        }
-      }
-    }
-    array = newArray;
+    Integer[] filteredArray = Arrays.stream(array).filter(elem -> !elem.equals(item)).toArray(Integer[]::new);
+    array = filteredArray;
     return true;
   }
 }
